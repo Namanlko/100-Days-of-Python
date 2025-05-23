ed4d8d916d99aadccf5fae925dffcaa260d1ea95
Namanlko <namanpandeylko32@gmail.com>
2025-05-23 10:35:34 +0530

    Completed Day 5. Comment, Escape Sequence & Print Function

---
+++ Day 5 Comment, Escape Sequenec & Print Function/main.py
@@ -0,0 +1,26 @@
+# 1. Single Line Comment:
+print("Hello World!") # It will print Hello World on console.
+
+# 2. Multi Line Comment:
+
+print("Name: Rohan")
+print("Language: Python")
+print("Company: Genpact")
+
+'''
+Output:
+Name: Rohan
+Language: Python
+Company: Genpact
+'''
+
+# 3. Escape Sequence Character:
+# print("This doesnt "execute") -> It will show error.
+print("This will \" execute")
+
+# 4. More on print Statement:
+
+print("Hello",1,2,3)
+print("Hello",1,2,3,sep="-")
+print("Hello",1,2,3,sep="~", end='\n')
+print("Hello",1,2,3,sep=">", end='12389')
