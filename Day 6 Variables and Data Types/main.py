dcfab79cec3e0f119b7fe51a46090ce3cea073d1
Namanlko <namanpandeylko32@gmail.com>
2025-05-25 02:03:17 +0530

    Completed Day 6. Variables and Data Types

---
+++ Day 6 Variables and Data Types/main.py
@@ -0,0 +1,59 @@
+# 1. Variables in Python
+
+a = 1
+b = True
+c = "John"
+d = None
+e = complex(8,2)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# 2. Type Function - Used to identify the data type of a vaiarble.
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+# 3. Numeric Data Type - Int, Float & Complex.
+
+num1 = 1
+num2 = 3.14
+num3 = complex(3,4)
+
+# 4. Text Data Type - String.
+
+str1 = "Hello World!"
+str2 = "Python Programming"
+
+# 5. Boolean Data Type - String.
+
+flag = True
+newFlag  = False
+
+# 6. Sequence Data Type - List, Tuple.
+
+# List Data Type - Mutable.
+list1 = [1,2,3,4,5]
+list2 = [1,'a',"Hello"]
+list3 = [5,[1,2,3],'B',"python"]
+
+# Tuple Data Type - Immutable.
+list1 = (1,2,3,4,5)
+list2 = (1,'a',"Hello")
+list3 = (5,[1,2,3],'B',"python")
+
+# 7. Mapped Data Type - Dictionary.
+
+dict = {
+    "name": "John",
+    "age": 20,
+    "canVote": True
+}
+
+
