b8c58d7f02b2931238cfcbd6b48fecdfe42bb991
Namanlko <namanpandeylko32@gmail.com>
2025-05-29 12:57:24 +0530

    Completed Day 10. Taking User Input

---
+++ Day 10 Taking User Input/main.py
@@ -0,0 +1,11 @@
+# 1. Input Function: Input Function in python is used to take imput from the user. It always take string as input and we have to type caste it into another data type as per the requirement.
+
+# Example 1:
+a = input("Enter Your Name: ")
+print("My name is", a)
+
+# Example 2:
+x = input("Enter First Number: ")
+y = input("Enter Second Number: ")
+print(x  + y)           # Without Type Casting
+print(int(x) + int(y))  # With Type Casting
