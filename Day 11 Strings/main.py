3d45c456e4d3262c973a876f9d1f037d72d2990e
Namanlko <namanpandeylko32@gmail.com>
2025-05-30 09:06:59 +0530

    Completed Day 11. Strings

---
+++ Day 11 Strings/main.py
@@ -0,0 +1,25 @@
+# 1. String 
+name = "John"
+friend = "Rohan"
+anotherFriend = 'Lovish'
+
+# 2. String with Line Breaks.
+apple = '''He said, 
+Hi Harry
+hey I am good
+"I want to eat an apple'''
+
+# 3. String with Print Function.
+print("Hello, " + name)
+print(apple) 
+
+# 4. String Indexing. In Python indexing always start with 0 and python also as negative indexing.
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+# 5. Looping in String.
+for character in apple:
+    print(character)
