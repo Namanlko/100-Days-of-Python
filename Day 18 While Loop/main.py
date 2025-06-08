15113a62940714a708b44454301585878145be51
Namanlko <namanpandeylko32@gmail.com>
2025-06-09 01:06:51 +0530

    Completed Day 18. While Loop

---
+++ Day 18 While Loop/main.py
@@ -0,0 +1,21 @@
+# 1. While Loop
+
+i = int(input("Enter the number: "))
+print(i)
+while(i<=38):
+  i = int(input("Enter the number: "))
+  print(i)
+print("Done with the loop")
+
+# 2. While Loop (Decrementing)
+count = 5
+while (count > 0):
+  print(count)
+  count = count - 1
+else:
+  print("I am inside else")
+
+# 3. Do While Loop
+# do {
+#   loop body;
+# }while(condition);
