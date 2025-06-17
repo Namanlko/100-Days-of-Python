237365ed83ff2ee6fba1078c5d5d040236701363
Namanlko <namanpandeylko32@gmail.com>
2025-06-17 08:13:21 +0530

    Completed Day 24. Introduction To Tuples

---
+++ Day 24 Introduction To Tuples/main.py
@@ -0,0 +1,17 @@
+# 1. Tuple in Python: 
+tup = (1, 2, 76, 342, 32, "green", True)
+print(type(tup), tup)
+
+# 2. Len Fuction in Tuple:
+print(len(tup))
+
+# 3. Tuple Indexing:
+print(tup[0])
+print(tup[-1])
+print(tup[2])
+
+# 4. If condition in Tuple:
+if  3421 in tup:
+  print("Yes 342 is present in this tuple")
+tup2 = tup[1:4]
+print(tup2)
