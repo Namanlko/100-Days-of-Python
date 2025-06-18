25eabfdb573ee8eb95d4e865862d22907c592e22
CodeWithAbhiPandey <abhishekpandey4028@gmail.com>
2025-06-18 20:31:04 +0530

    Completed Day 25. Operations on Tuples

---
+++ Day 25 Operations on Tuples/main.py
@@ -0,0 +1,14 @@
+# 1. Tuple in Python:
+tuple1 = (0, 1, 2, 3, 2, 31, 1, 3, 2, 3)
+
+# 2. Count Function: Count Function is used to return the count of the number of occurenece of a given number.
+ans1 = tuple1.count(3)
+print(ans1)
+
+# 3. Index Function: Index Function is used to return the first occurenece of a given number.
+ans2 = tuple1.index(3)
+print(ans2)
+
+# 4. Index Function: Example:
+ans3 = tuple1.index(31)
+print(ans3)
